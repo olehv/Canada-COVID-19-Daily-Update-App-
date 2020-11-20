34eac080eefd1cf379ee2fe8c7fba1139d5a86cf
oleh <oleh.vytvitskyy@gmail.com>
2020-11-20 10:35:46 -0500

    Initial Commit

---
+++ Assignment2_Oleh_Vytvitskyy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2_Oleh_Vytvitskyy
+//
+//  Created by oleh on 2020-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
